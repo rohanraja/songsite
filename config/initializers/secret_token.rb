a2026e692ad7a81e44b4ee207395f867c6c8abda
rohan1020 <rohanraja9@gmail.com>
2013-10-28 11:28:18 +0530

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songsite::Application.config.secret_key_base = '27a60646d6b149f8cd6b91fd1ab16e4c1208ad69bf2b00940b52eb9a4d1cd2e09ac92a3f1c935f0ca545855d13d4724951de25e19f78055eee8ff07cbd429a47'
